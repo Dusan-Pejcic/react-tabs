4412f26fa42f08a6d4a38666649d8093416b7849
dusan pejcic <dushan.pejcic@googlemail.com>
2021-09-20 00:24:16 +0200

    fetch function and useEffect hook added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async ()=> {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(()=> {
+    fetchJobs();
+  }, []);
+
+  if(loading){
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+
   return (
     <div className="App">
-      
+      <h1>react tabs</h1>
     </div>
   );
 }
