cbfbeac7fb2bbadcc6e56cb103d7be34cfbabd8b
dusan pejcic <dushan.pejcic@googlemail.com>
2021-09-20 00:45:15 +0200

    the markup to display single entry added

--- src/App.js
+++ src/App.js
@@ -26,11 +26,31 @@ function App() {
       </section>
     )
   }
-
+  // destructure after jobs are loaded
+  const {id, order, title, dates, duties, company} = jobs[value];
   return (
-    <div className="App">
-      <h1>react tabs</h1>
-    </div>
+    <section className='section'>
+      <div className="title">
+        <h2>expirience</h2>
+        <div className="underline"></div>  
+      </div>
+      <div className="jobs-center">
+        { /* btn container */}
+        { /* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p>{dates}</p>
+          {duties.map((duty, index)=> {
+            return  <div key={index} className="job-desc">
+              <FaAngleDoubleRight className='job-icon'/>
+              <p>{duty}</p>
+            </div>   
+          })};
+        </article>
+
+      </div>
+    </section>
   );
 }
 
